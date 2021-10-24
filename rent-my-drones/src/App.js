ac1f24fb1f6c5ba2002a0ee283403e64380f79d3
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-23 21:19:54 -0500

    added imports and Switch comp

--- rent-my-drones/src/App.js
+++ rent-my-drones/src/App.js
@@ -1,12 +1,18 @@
 import React from'react'
 import UnsignedNavigation from './components/UnsignedNavigation';
+import {Switch,Route} from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <UnsignedNavigation/>
 
-      
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
+
+
     
     </div>
   );
