e08a86fabd68c4dc3b34b681ded512edcb492ff4
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-23 21:14:19 -0500

    nav semi complete

--- rent-my-drones/src/components/UnsignedNavigation.js
+++ rent-my-drones/src/components/UnsignedNavigation.js
@@ -1,6 +1,7 @@
 import { Menu } from 'antd'
 import React from "react";
 import {Link} from 'react-router-dom'
+import {HomeOutlined,PlusCircleOutlined,LoginOutlined} from '@ant-design/icons'
 
 
 
@@ -9,9 +10,18 @@ const UnsignedNavigation=()=>{
 
     return(
         <Menu mode='horizontal'>
-           <Menu.Item><Link to='/'>Home</Link></Menu.Item>
-           <Menu.Item><Link to = '/SignUp'>Sign Up</Link></Menu.Item>
-           <Menu.Item><Link to ='/Login'>Login</Link></Menu.Item>
+           <Menu.Item>
+               <Link to='/'><HomeOutlined/></Link>
+               <h2>Home</h2>
+           </Menu.Item>
+           <Menu.Item>
+               <Link to = '/SignUp'><PlusCircleOutlined/></Link>
+               <h2>SignUp</h2>
+           </Menu.Item>
+           <Menu.Item>
+               <Link to ='/Login'><LoginOutlined/></Link>
+               <h2>Login</h2>
+           </Menu.Item>
         </Menu>
     )
 
