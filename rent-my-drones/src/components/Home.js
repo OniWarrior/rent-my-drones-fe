8e4ab7f72107342d55b8544eff6c4ff8fc4add5d
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-26 20:12:48 -0500

    More style changes

--- rent-my-drones/src/components/Home.js
+++ rent-my-drones/src/components/Home.js
@@ -7,7 +7,8 @@ const Home =()=>{
         <div className='home-container'>
             <div className='drone-img'>
             </div>                
-            <main className='main-doc'>           
+            <main className='main-doc'>    
+                <h1>Rent My Drone</h1>       
 
                 <p>Here at Rent My Drone, we allow the drone enthusiast to directly rent from a local drone owner.</p>
                 <p>We also allow an owner to rent out their drone to earn money</p>
