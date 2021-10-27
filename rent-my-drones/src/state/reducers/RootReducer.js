69ae19cdb30636c1edbda604d356cbc7faed7773
Stephen Aranda <aranda.stephen88@gmail.com>
2021-10-26 21:09:26 -0500

    completed root reducer

--- rent-my-drones/src/state/reducers/RootReducer.js
+++ rent-my-drones/src/state/reducers/RootReducer.js
@@ -1,1 +1,6 @@
 import { combineReducers} from 'redux'
+import signupReducer from './SignupReducer'
+import loginReducer from './LoginReducer'
+
+const rootReducer = combineReducers({signupReducer,loginReducer})
+export default rootReducer
