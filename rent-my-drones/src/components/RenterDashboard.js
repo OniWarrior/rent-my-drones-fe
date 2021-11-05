e19495a380d39b9a8e5cd0e0641e2a26e0c996d1
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-05 12:59:30 -0500

    Created header for dashboard

--- rent-my-drones/src/components/RenterDashboard.js
+++ rent-my-drones/src/components/RenterDashboard.js
@@ -7,6 +7,7 @@ const RenterDashboard=()=>{
     return(
         <div className='dashboard-container'>
             <div className='dashboard-header'>
+                <h1>Dashboard</h1>                
             </div>
             <Button className='dashboard-card'></Button>
             <Button className='dashboard-card'></Button>
