ee0998ea55d2385f2d5ddae18cedc162f269f20d
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-05 10:08:01 -0500

    Added rented card and changed navigation

--- rent-my-drones/src/components/RentedCard.js
+++ rent-my-drones/src/components/RentedCard.js
@@ -6,6 +6,8 @@ const RentedCard=(props)=>{
     return(
         <div className="rented-item-card">
             <img src={props.image} alt='Rented item'/>
+            <h2>{props.itemName}</h2>
+            
 
         </div>
     )
