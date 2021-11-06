a65c9c0f4672e90dea6295d143c1cf7eb512779f
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-06 06:49:26 -0500

    created blank function for logged in navigation

---
+++ rent-my-drones/src/components/LoggedInNavigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const LoggedInNavigation=()=>{
+    return(
+        <div className='nav-container'>
+            
+        </div>
+    )
+
+}
+
+export default LoggedInNavigation
