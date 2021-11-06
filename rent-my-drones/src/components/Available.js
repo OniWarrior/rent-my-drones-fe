0e54879abfccc1fad763615391ca7133c5141b6c
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-06 12:07:38 -0500

    put in nav comp and header

--- rent-my-drones/src/components/Available.js
+++ rent-my-drones/src/components/Available.js
@@ -1,7 +1,17 @@
 import React from "react";
+import LoggedInNavigation from "./LoggedInNavigation";
 
 
 const Available=()=>{
+    return (
+        <div className='available-container'>
+            <LoggedInNavigation/>
+            <div className='available-header'>
+                <h1>Available Drones</h1>
+            </div>
+
+        </div>
+    )
 
 }
 
