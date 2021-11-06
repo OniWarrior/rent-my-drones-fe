3561e53a1263d84894a3275b50a30f04f8d2cace
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-06 06:56:33 -0500

    created const for history hook

--- rent-my-drones/src/components/LoggedInNavigation.js
+++ rent-my-drones/src/components/LoggedInNavigation.js
@@ -1,16 +1,16 @@
 import React from "react";
 import {Row, Button } from 'antd';
+import { useHistory } from "react-router";
 
 const LoggedInNavigation=()=>{
+    const {push} = useHistory()
     return(
         <div className='nav-container'>
             <Row className='nav-bar'>
                 <Button>Dashboard</Button>
                 <Button>Available</Button>
                 <Button>Rented</Button>
-
             </Row>
-
         </div>
     )
 
