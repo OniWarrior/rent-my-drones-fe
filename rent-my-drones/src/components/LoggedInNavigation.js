4e23833a3df1e344b0f52019f8daca7a1420051a
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-06 07:01:04 -0500

    inserted handlers into buttons onClick listener

--- rent-my-drones/src/components/LoggedInNavigation.js
+++ rent-my-drones/src/components/LoggedInNavigation.js
@@ -10,21 +10,21 @@ const LoggedInNavigation=()=>{
         push('/Dashboard')
     }
 
-    const gotToAvailable=(e)=>{
+    const goToAvailable=(e)=>{
         e.preventDefault()
         push('/avaliable')
     }
 
-    const gotToRented=(e)=>{
+    const goToRented=(e)=>{
         e.preventDefault()
         push('/rented')
     }
     return(
         <div className='nav-container'>
             <Row className='nav-bar'>
-                <Button>Dashboard</Button>
-                <Button>Available</Button>
-                <Button>Rented</Button>
+                <Button onClick={goToDashboard}>Dashboard</Button>
+                <Button onClick={goToAvailable}>Available </Button>
+                <Button onClick={goToRented}>Rented</Button>
             </Row>
         </div>
     )
