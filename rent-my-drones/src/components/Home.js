baf2ad2ae0ad2f23b7c6af71ad6255a3e4114db3
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-09 12:49:33 -0600

    corrected spelling to h1 text

--- rent-my-drones/src/components/Home.js
+++ rent-my-drones/src/components/Home.js
@@ -11,7 +11,7 @@ const Home =()=>{
             <div className='drone-img'>
             </div>                
             <main className='main-doc'>    
-                <h1>Rent My Drone</h1>       
+                <h1>Rent My Drones</h1>       
 
                 <p>Here at Rent My Drone, we allow the drone enthusiast to directly rent from a local drone owner.</p>
                 <p>We also allow an owner to rent out their drone to earn money</p>
