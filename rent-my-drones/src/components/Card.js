a55bfc908382f4ae975e38b2e6856cdb73f0095a
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 14:56:15 -0600

    added drone description

--- rent-my-drones/src/components/Card.js
+++ rent-my-drones/src/components/Card.js
@@ -7,6 +7,7 @@ const Card=(props)=>{
         <div className="item-card">
             <img src={props.image} alt='Card of item'/>
             <h2>{props.drone_name}</h2>
+            <p>{props.drone_description}</p>
             <p>{props.drone_cost}</p>
             
 
