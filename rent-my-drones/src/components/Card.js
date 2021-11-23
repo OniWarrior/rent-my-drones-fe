4b1bbf56cec956dc842764d12044b7928a03fea1
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 14:58:34 -0600

    changed image to drone_image

--- rent-my-drones/src/components/Card.js
+++ rent-my-drones/src/components/Card.js
@@ -5,7 +5,7 @@ const Card=(props)=>{
 
     return(
         <div className="item-card">
-            <img src={props.image} alt='Card of item'/>
+            <img src={props.drone_image} alt='Card of item'/>
             <h2>{props.drone_name}</h2>
             <p>{props.drone_description}</p>
             <p>{props.drone_cost}</p>
