e255030b97897267926874371fc583d90e8efce6
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 16:27:53 -0600

    finished defining schema

--- rent-my-drones/src/formSchemas/SignupFormSchema.js
+++ rent-my-drones/src/formSchemas/SignupFormSchema.js
@@ -2,6 +2,14 @@ import * as yup from 'yup'
 
 
 const SignUpFormSchema= yup.object().shape({
+    username:yup.string()
+     .trim()
+     .required("Username is required. Please fill out field")
+     .min(12,'A minimum of 12 characters is required'),
+    password:yup.string()
+     .trim()
+     .required("Password is required. Please fill out field")
+     .min(12,'A minimum of 12 characters is required for password')
 
 })
 
