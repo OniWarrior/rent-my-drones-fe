5312326e89c865c0ea4f3eaf874307c64abe46de
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 14:52:28 -0600

    changed class name to item card

--- rent-my-drones/src/components/Card.js
+++ rent-my-drones/src/components/Card.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Card=(props)=>{
 
     return(
-        <div className="rented-item-card">
+        <div className="item-card">
             <img src={props.image} alt='Rented item'/>
             <h2>{props.itemName}</h2>
             
