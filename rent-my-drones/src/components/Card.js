ff6d71ad582d92e19a8a34e11e806ab6daf5444c
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 14:53:06 -0600

    changed alt atrr text

--- rent-my-drones/src/components/Card.js
+++ rent-my-drones/src/components/Card.js
@@ -5,7 +5,7 @@ const Card=(props)=>{
 
     return(
         <div className="item-card">
-            <img src={props.image} alt='Rented item'/>
+            <img src={props.image} alt='Card of item'/>
             <h2>{props.itemName}</h2>
             
 
