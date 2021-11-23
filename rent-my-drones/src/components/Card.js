9db794840aa90a962b5d56a8300031954ed91c72
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 15:01:37 -0600

    removed empty space

--- rent-my-drones/src/components/Card.js
+++ rent-my-drones/src/components/Card.js
@@ -9,8 +9,6 @@ const Card=(props)=>{
             <h2>{props.drone_name}</h2>
             <p>{props.drone_description}</p>
             <p>{props.drone_cost}</p>
-            
-
         </div>
     )
 
