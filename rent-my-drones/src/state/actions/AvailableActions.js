1222ec24202376ecd7a411336c78035f20adfc40
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 21:29:40 -0600

    created request to get available drones

--- rent-my-drones/src/state/actions/AvailableActions.js
+++ rent-my-drones/src/state/actions/AvailableActions.js
@@ -3,3 +3,8 @@ import axiosWitAuth from '../../components/utils/AxiosWithAuth'
 export const AVAILABLE_START = 'AVAILABLE_START'
 export const AVAILABLE_SUCCESS = 'AVAILABLE_SUCCESS'
 export const AVAILABLE_FAILURE = 'AVAILABLE_FAILURE'
+
+
+export const getAvailableDrones=()=>(dispatch)=>{
+    
+}
