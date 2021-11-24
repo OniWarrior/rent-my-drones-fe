36d76ae405043a97c118e1f7250d0425cd98742a
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 20:39:42 -0600

    imported css file for comp

--- rent-my-drones/src/components/Rented.js
+++ rent-my-drones/src/components/Rented.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "antd";
 import LoggedInNavigation from "./LoggedInNavigation";
-
+import '../styles/Rented.css'
 
 const Rented =()=>{
     return (
