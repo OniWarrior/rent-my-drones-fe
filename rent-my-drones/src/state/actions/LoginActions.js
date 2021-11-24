21438ca2155fd1b990b46fc981f6da65a578da18
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 20:19:52 -0600

    corrected post for login

--- rent-my-drones/src/state/actions/LoginActions.js
+++ rent-my-drones/src/state/actions/LoginActions.js
@@ -1,4 +1,5 @@
-import axiosWithAuth from "../../components/utils/AxiosWithAuth"
+import axios from "axios"
+
 
 export const LOGIN_START="LOGIN_START"
 export const LOGIN_SUCCESS='LOGIN_SUCCESS'
@@ -8,14 +9,14 @@ export const LOGIN_FAILURE='LOGIN_FAILURE'
 export const postLogin=(e,login,push)=>(dispatch)=>{
     e.preventDefault()
     dispatch({type:LOGIN_START})
-    axiosWithAuth(`https://rent-my-drones-be.herokuapp.com/api/auth/Login`,login)
+    axios.post(`https://rent-my-drones-be.herokuapp.com/api/auth/Login`,login)
     .then((success)=>{
         dispatch({type:LOGIN_SUCCESS,payload:success.data})
         push('/Dashboard')          
     })
     .catch(err=>{
         dispatch({type:LOGIN_FAILURE,payload:err.message})
-        
+
     })
 
 }
