db84f48e382a6e74eed7da19b1433a6ab49a9da3
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-24 16:08:31 -0600

    added Image import

--- rent-my-drones/src/components/Available.js
+++ rent-my-drones/src/components/Available.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Button,Card } from "antd";
+import { Button,Card ,Image} from "antd";
 import LoggedInNavigation from "./LoggedInNavigation";
 import '../styles/Available.css'
 import { connect } from "react-redux";
