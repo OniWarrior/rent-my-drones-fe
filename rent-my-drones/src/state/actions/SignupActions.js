1afef5aa196bf1e40c866701dbf974a63a849fd6
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 19:22:25 -0600

    created register http request

--- rent-my-drones/src/state/actions/SignupActions.js
+++ rent-my-drones/src/state/actions/SignupActions.js
@@ -4,3 +4,8 @@ export const SIGNUP_START = 'SIGNUP_START'
 export const SIGNUP_SUCCESS='SIGNUP_SUCCESS'
 export const SIGNUP_FAILURE='SIGNUP_FAILURE'
 
+
+export const register=(signup,push)=>(dispatch)=>{
+    
+}
+
