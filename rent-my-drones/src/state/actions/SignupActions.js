97aa2367261cff0deadbd3d2a2213d99fa7a5b59
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 19:39:12 -0600

    created happy path for register

--- rent-my-drones/src/state/actions/SignupActions.js
+++ rent-my-drones/src/state/actions/SignupActions.js
@@ -8,6 +8,9 @@ export const SIGNUP_FAILURE='SIGNUP_FAILURE'
 export const register=(signup,push)=>(dispatch)=>{
     dispatch({type:SIGNUP_START})
     axios.post(`https://rent-my-drones-be.herokuapp.com/api/auth/Signup`,signup)
+    .then(success =>{
+        
+    })
 
 }
 
