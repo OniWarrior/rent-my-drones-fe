4de92edd9375254d89ea9cba87944ee4a7277441
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 19:32:40 -0600

    created post request for registering account

--- rent-my-drones/src/state/actions/SignupActions.js
+++ rent-my-drones/src/state/actions/SignupActions.js
@@ -7,6 +7,7 @@ export const SIGNUP_FAILURE='SIGNUP_FAILURE'
 
 export const register=(signup,push)=>(dispatch)=>{
     dispatch({type:SIGNUP_START})
+    axios.post(`https://rent-my-drones-be.herokuapp.com/api/auth/Signup`,signup)
 
 }
 
