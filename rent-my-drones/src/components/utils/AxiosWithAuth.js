72360f60cbefa8f7bbf7aa1a8c47721ccfaa9b2e
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 19:37:30 -0600

    loaded authorization header with token

--- rent-my-drones/src/components/utils/AxiosWithAuth.js
+++ rent-my-drones/src/components/utils/AxiosWithAuth.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
 const axiosWithAuth=()=>{
-    
+    return axios.create({
+        baseURL: 'https://rent-my-drones-be.herokuapp.com/',
+        headers:{
+            authorizaion: localStorage.getItem('token')
+        }
+
+    })
 }
