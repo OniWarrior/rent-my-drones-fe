88acf66b5020a8fd568fa51c40a1bd0cd71c815d
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-23 19:53:27 -0600

    imported Input and Form comp

--- rent-my-drones/src/components/LoginPage.js
+++ rent-my-drones/src/components/LoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/Login.css'
-import { Button } from 'antd'
+import { Input,Form,Button } from "antd";
 import { Link } from 'react-router-dom'
 import UnsignedNavigation from './UnsignedNavigation'
 
