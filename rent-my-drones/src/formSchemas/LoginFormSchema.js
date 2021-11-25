c600a1259fc5d3036cb0309c51773c52bca01bf9
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-25 15:00:50 -0600

    added validation for password

--- rent-my-drones/src/formSchemas/LoginFormSchema.js
+++ rent-my-drones/src/formSchemas/LoginFormSchema.js
@@ -1,9 +1,12 @@
+import Password from 'antd/lib/input/Password'
 import * as yup from 'yup'
 
 
 const LoginFormSchema = yup.object().shape({
     username:yup.string()
      .trim()
-     .required("Username is required. Please fill out field")
-
+     .required("Username/Password is required. Please fill out field"),
+    password:yup.string()
+     .trim()
+     .required("Username/Password is required. Please fill out field")
 })
