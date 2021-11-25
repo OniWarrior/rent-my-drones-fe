680a96bc8c3cfde74ad907ee67fb39b872fab46e
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-24 22:16:33 -0600

    created http request for rented drone retrieval

--- rent-my-drones/src/state/actions/RentedActions.js
+++ rent-my-drones/src/state/actions/RentedActions.js
@@ -2,3 +2,8 @@ import axiosWithAuth from "../../components/utils/AxiosWithAuth";
 
 export const RENTED_START='RENTED_START'
 export const RENTED_SUCCESS='RENTED_SUCCESS'
+export const RENTED_FAILURE='RENTED_FAILURE'
+
+export const getRentedDrones=()=>(dispatch)=>{
+    
+}
