e968c5f997642076756d75926db31cdfb6cb7b26
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-24 19:23:51 -0600

    import useState

--- rent-my-drones/src/components/Available.js
+++ rent-my-drones/src/components/Available.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { Button,Card} from "antd";
 import LoggedInNavigation from "./LoggedInNavigation";
 import '../styles/Available.css'
