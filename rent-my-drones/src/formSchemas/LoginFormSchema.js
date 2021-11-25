f72a28523d7db97cacbf747a432112f8acae6f92
Stephen Aranda <aranda.stephen88@gmail.com>
2021-11-25 14:58:52 -0600

    added validation for username

--- rent-my-drones/src/formSchemas/LoginFormSchema.js
+++ rent-my-drones/src/formSchemas/LoginFormSchema.js
@@ -2,5 +2,8 @@ import * as yup from 'yup'
 
 
 const LoginFormSchema = yup.object().shape({
-    
+    username:yup.string()
+     .trim()
+     .required("Username is required. Please fill out field")
+
 })
