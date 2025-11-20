ac3d226d7fdc070b64d0aa0088ac93c32afce1c4
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-20 13:18:13 -0600

    updated Switch to Routes

--- rent-my-drones/src/App.js
+++ rent-my-drones/src/App.js
@@ -1,5 +1,5 @@
-import React from'react'
-import {Switch,Route} from 'react-router-dom'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home';
 import LoginPage from './components/LoginPage';
 import RenterDashboard from './components/RenterDashboard';
@@ -12,17 +12,17 @@ import Rented from './components/Rented';
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/Login' component={LoginPage}/>
-        <Route path='/Signup' component={Signup}/>
-        <ProtectedRoute exact path='/Dashboard' component={RenterDashboard}/>
-        <ProtectedRoute path='/Dashboard/available' component={Available}/>
-        <ProtectedRoute path='/Dashboard/rented' component={Rented}  />      
-      </Switch>
+      <Routes>
+        <Route exact path='/' component={Home} />
+        <Route path='/Login' component={LoginPage} />
+        <Route path='/Signup' component={Signup} />
+        <ProtectedRoute exact path='/Dashboard' component={RenterDashboard} />
+        <ProtectedRoute path='/Dashboard/available' component={Available} />
+        <ProtectedRoute path='/Dashboard/rented' component={Rented} />
+      </Routes>
 
 
-    
+
     </div>
   );
 }
