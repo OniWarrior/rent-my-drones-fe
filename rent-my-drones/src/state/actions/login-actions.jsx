4b0bf7286624d012612b259fca909408c50c26b0
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-20 14:38:48 -0600

    refactored api call

--- rent-my-drones/src/state/actions/login-actions.jsx
+++ rent-my-drones/src/state/actions/login-actions.jsx
@@ -1,24 +1,34 @@
 import axios from "axios"
 
 
-export const LOGIN_START="LOGIN_START"
-export const LOGIN_SUCCESS='LOGIN_SUCCESS'
-export const LOGIN_FAILURE='LOGIN_FAILURE'
+export const LOGIN_START = "LOGIN_START"
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
 
 
-export const postLogin=(login,push)=>(dispatch)=>{
-    
-    dispatch({type:LOGIN_START})
-    axios.post(`https://rent-my-drones-be.herokuapp.com/api/auth/Login`,login)
-    .then(response=>{
-        dispatch({type:LOGIN_SUCCESS,payload:response.data})
-        localStorage.setItem('token', response.data.token)
-        push('/Dashboard')          
-    })
-    .catch(err=>{
-        dispatch({type:LOGIN_FAILURE,payload:err.message})
+export const postLogin = (login, push) => async (dispatch) => {
+    try {
+        // dispatch start of action to reducer
+        dispatch({ type: LOGIN_START });
 
-    })
+        // make api call and save response 
+        // TODO - update this endpoint after backend is redeployed
+        const response = await axios.post(`https://rent-my-drones-be.herokuapp.com/api/auth/Login`, login);
+
+        // dispatch success to reducer
+        dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+
+        // store token
+        localStorage.setItem('token', response.data.token);
+
+        // nav to dashboard
+        push('/dashboard');
+
+
+    } catch (err) {
+        // dispatch failure to reducer
+        dispatch({ type: LOGIN_FAILURE, payload: err.message });
+    }
 
 }
 
