f732a8613a50beda40f847f73d48ab1e95a2ddff
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-20 14:44:14 -0600

    refactored api calls

--- rent-my-drones/src/state/actions/rented-actions.jsx
+++ rent-my-drones/src/state/actions/rented-actions.jsx
@@ -1,29 +1,45 @@
 import axiosWithAuth from "../../components/utils/AxiosWithAuth";
 
-export const RENTED_START='RENTED_START'
-export const RENTED_SUCCESS='RENTED_SUCCESS'
-export const RENTED_FAILURE='RENTED_FAILURE'
+export const RENTED_START = 'RENTED_START'
+export const RENTED_SUCCESS = 'RENTED_SUCCESS'
+export const RENTED_FAILURE = 'RENTED_FAILURE'
 
-export const getRentedDrones=()=>(dispatch)=>{
-    dispatch({type:RENTED_START})
-    axiosWithAuth().get(`/api/users/rented`)
-    .then(success=>{
-        dispatch({type:RENTED_SUCCESS,payload:success.data})
-    })
-    .catch(err=>{
-        dispatch({type:RENTED_FAILURE,payload:err.message})
-    })
+export const getRentedDrones = () => async (dispatch) => {
+    try {
+
+        // dispatch start of action to reducer
+        dispatch({ type: RENTED_START });
+
+        // make api call and save response
+        const response = await axiosWithAuth().get(`/api/users/rented`);
+
+        // dispatch success to reducer
+        dispatch({ type: RENTED_SUCCESS, payload: response.data });
+
+    } catch (err) {
+        //dispatch failure to reducer
+        dispatch({ type: RENTED_FAILURE, payload: err.message });
+    }
 
 }
 
-export const returnRentedDrone=(id)=>(dispatch)=>{
-    dispatch({type:RENTED_START})
-    axiosWithAuth().put(`/api/users/rented/${id}`)
-    .then(success=>{
-        dispatch({type:RENTED_SUCCESS,payload:success.data})
-    })
-    .catch(err=>{
-        dispatch({type:RENTED_FAILURE,payload:err.message})
-    })
+export const returnRentedDrone = (id) => async (dispatch) => {
+
+    try {
+        //dispatch start of action to reducer
+        dispatch({ type: RENTED_START });
+
+        // make api call and save response
+        const response = await axiosWithAuth().put(`/api/users/rented/${id}`);
+
+        //dispatch success to reducer
+        dispatch({ type: RENTED_SUCCESS, payload: response.data })
+
+
+    } catch (err) {
+
+        // dispatch failure to reducer
+        dispatch({ type: RENTED_FAILURE, payload: err.message });
+    }
 
 }
