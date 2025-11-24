9e02af04ae4e5fbec6db878abcd1bd60af8505ea
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-24 16:42:03 -0600

    refactored home comp with containers

--- rent-my-drones/src/components/home.jsx
+++ rent-my-drones/src/components/home.jsx
@@ -5,22 +5,29 @@ import UnsignedNavigation from './unsigned-navigation'
 const Home = () => {
 
     return (
-        <div>
+        <div className='home'>
             <UnsignedNavigation />
             <div className='home-container'>
                 <div className='drone-img'>
-                </div>
-                <main className='main-doc'>
-                    <h1>Rent My Drones</h1>
+                    <div className='hero-text-container'>
+                        <div className='hero-text-header'>
+                            <h1>Rent My Drones</h1>
+                        </div>
 
-                    <p>Here at Rent My Drone, we allow the drone enthusiast to directly rent from a local drone owner.</p>
-                    <p>We also allow an owner to rent out their drone to earn money</p>
-                    <p>So if your a drone owner lookign to earn some extra money by renting out your drone or a drone enthusiast who needs a drone quickly for use.
-                        then signup/login at the top of the page.
-                    </p>
-                </main>
+                        <div className='hero-text'>
+                            <p>Here at Rent My Drones, we allow the drone enthusiast to directly rent from a local drone owner.
+                                We also allow an owner to rent out their drone to earn money.
+                                So if your a drone owner looking to earn some extra money by renting out your drone or a drone enthusiast who needs a drone quickly for use,
+                                then signup/login at the top of the page. </p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
-        </div>
+        </div >
     )
 
 
