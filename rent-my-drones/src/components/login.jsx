6ca6b5c984fb35ceaff4f862e1aba23abbb8cc3e
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-25 11:21:47 -0600

    compl component

--- rent-my-drones/src/components/login.jsx
+++ rent-my-drones/src/components/login.jsx
@@ -10,7 +10,7 @@ import UnsignedNavigation from './unsigned-navigation';
 import LoginFormSchema from '../formSchemas/login-form-schema';
 import { useValidation } from "../hooks/useValidation";
 import { useNavigate } from 'react-router-dom';
-import { useState } from "react";
+
 import { connect } from "react-redux";
 import { postLogin } from '../state/actions/login-actions';
 
@@ -19,11 +19,10 @@ import { postLogin } from '../state/actions/login-actions';
 
 const LoginPage = (props) => {
 
-  // State vars for navigation, button disabling, errors, and login
+  // State vars for navigation, errors, and login
   const navigate = useNavigate();
   const [login, errors, setLogin] = useValidation(LoginFormSchema);
-  const initialDisabled = true;
-  const [disabled, setDisabled] = useState(initialDisabled);
+
 
   // handler for changing input in text boxes
   const onchangeInput = (event) => {
@@ -31,20 +30,7 @@ const LoginPage = (props) => {
   }
 
 
-  // handler for disabling the login button.
-  const handleDisabled = (e) => {
-    e.preventDefault();
-    if (login.username.length >= 12 && login.password.length >= 12) {
-      setDisabled(() => ({
-        disabled: !disabled
-      }))
-    }
-    else {
-      setDisabled(() => ({
-        disabled: disabled
-      }))
-    }
-  }
+
 
   // handler for form submission after login button is pressed.
   const onFormSubmit = () => {
@@ -57,49 +43,53 @@ const LoginPage = (props) => {
     <div className='login'>
       <UnsignedNavigation />
       <div className="login-main">
-        <div className='login-container'>
-          <div className='login-header'>
-            <h1>Login</h1>
+        <div className='login-img'>
+          <div className='login-container'>
+            <div className='login-header'>
+              <h1>Login</h1>
+            </div>
+            <form className='login-form' onSubmit={onFormSubmit}>
+              <div className='input-group login-group'>
+                <div className='login-labels'>
+                  <h2>Email: </h2>
+                  <h2>Password: </h2>
+                </div>
+                <div className='login-txt-boxes'>
+                  <input
+                    id='email'
+                    type='text'
+                    name='email'
+                    required
+                    placeholder='Email'
+                    onChange={onchangeInput}
+                  />
+                  <input
+                    id='password'
+                    type='text'
+                    name='password'
+                    required
+                    placeholder='Password'
+                    onChange={onchangeInput}
+                  />
+                </div>
+              </div>
+              <div className='btn-container'>
+                <button className='btn-submit btn-login' >Login</button>
+              </div>
+
+              <div className='errors'>
+                <p>{errors.email}</p>
+                <p>{errors.password}</p>
+              </div>
+
+
+
+
+            </form>
           </div>
-          <form className='login-form' onSubmit={onFormSubmit}>
-            <div className='input-group login'>
-              <div className='login-labels'>
-                <h2>Email: </h2>
-                <h2>Password: </h2>
-              </div>
-              <div className='login-txt-boxes'>
-                <input
-                  id='email'
-                  type='text'
-                  name='email'
-                  required
-                  placeholder='Email'
-                  onChange={onchangeInput}
-                />
-                <input
-                  id='password'
-                  type='text'
-                  name='password'
-                  required
-                  placeholder='Password'
-                  onChange={onchangeInput}
-                />
-              </div>
-            </div>
-            <div className='btn-container'>
-              <button className='btn-submit btn-login' disabled={handleDisabled}>Login</button>
-            </div>
 
-            <div className='errors'>
-              <p>{errors.email}</p>
-              <p>{errors.password}</p>
-            </div>
-
-
-
-
-          </form>
         </div>
+
 
       </div>
     </div>
