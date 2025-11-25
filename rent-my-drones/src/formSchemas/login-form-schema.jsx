e4e2a309e8bb0096638340cbe9c2060eae6a2237
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-25 11:21:20 -0600

    changed property name

--- rent-my-drones/src/formSchemas/login-form-schema.jsx
+++ rent-my-drones/src/formSchemas/login-form-schema.jsx
@@ -3,12 +3,12 @@ import * as yup from 'yup'
 
 
 const LoginFormSchema = yup.object().shape({
-    username:yup.string()
-     .trim()
-     .required("Username/Password is required. Please fill out field"),
-    password:yup.string()
-     .trim()
-     .required("Username/Password is required. Please fill out field")
+    email: yup.string()
+        .trim()
+        .required("Email/Password is required. Please fill out field"),
+    password: yup.string()
+        .trim()
+        .required("Email/Password is required. Please fill out field")
 })
 
 export default LoginFormSchema
