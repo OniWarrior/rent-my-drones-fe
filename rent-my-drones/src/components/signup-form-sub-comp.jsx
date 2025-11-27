e594e014efe31d30fa7f314495504177abbb43d5
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-27 13:23:41 -0600

    created blank func for signup form container

--- rent-my-drones/src/components/signup-form-sub-comp.jsx
+++ rent-my-drones/src/components/signup-form-sub-comp.jsx
@@ -3,3 +3,9 @@
  * File   : signup-form-sub-comp.jsx 
  * Desc   : sub component fo signup.jsx. Represents the container for the form
  */
+
+const SignupFormContainer = () => {
+
+}
+
+export default SignupFormContainer;
