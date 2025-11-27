c62379dcf380ed9fdcb0b31735a9f64531674d9d
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-27 14:12:16 -0600

    added container for labels and text boxes

--- rent-my-drones/src/components/signup-form-sub-comp.jsx
+++ rent-my-drones/src/components/signup-form-sub-comp.jsx
@@ -41,46 +41,50 @@ const SignupFormContainer = (props) => {
                     <h1>Register New Account</h1>
                 </div>
                 <form className='signup-form' onSubmit={onFormSubmit}>
-                    <div className='signup-labels'>
-                        <h3>First Name:</h3>
-                        <h3>Last Name:</h3>
-                        <h3>email:</h3>
-                        <h3>password:</h3>
+                    <div className='signup-group'>
+                        <div className='signup-labels'>
+                            <h3>First Name:</h3>
+                            <h3>Last Name:</h3>
+                            <h3>email:</h3>
+                            <h3>password:</h3>
+                        </div>
+                        <div className='signup-txt-boxes'>
+                            <input
+                                id='first_name'
+                                type='text'
+                                name='first_name'
+                                required
+                                placeholder='First Name'
+                                onChange={onChangeInput}
+                            />
+                            <input
+                                id='last_name'
+                                type='text'
+                                name='last_name'
+                                required
+                                placeholder='Last Name'
+                                onChange={onChangeInput}
+                            />
+                            <input
+                                id='email'
+                                type='text'
+                                name='email'
+                                required
+                                placeholder='Email'
+                                onChange={onChangeInput}
+                            />
+                            <input
+                                id='password'
+                                type='text'
+                                name='password'
+                                required
+                                placeholder='Password'
+                                onChange={onChangeInput}
+                            />
+                        </div>
+
                     </div>
-                    <div className='signup-txt-boxes'>
-                        <input
-                            id='first_name'
-                            type='text'
-                            name='first_name'
-                            required
-                            placeholder='First Name'
-                            onChange={onChangeInput}
-                        />
-                        <input
-                            id='last_name'
-                            type='text'
-                            name='last_name'
-                            required
-                            placeholder='Last Name'
-                            onChange={onChangeInput}
-                        />
-                        <input
-                            id='email'
-                            type='text'
-                            name='email'
-                            required
-                            placeholder='Email'
-                            onChange={onChangeInput}
-                        />
-                        <input
-                            id='password'
-                            type='text'
-                            name='password'
-                            required
-                            placeholder='Password'
-                            onChange={onChangeInput}
-                        />
-                    </div>
+
                     <div className='signup-btn-container'>
                         <button type='submit' className='btn-submit btn-signup'>Register</button>
                     </div>
