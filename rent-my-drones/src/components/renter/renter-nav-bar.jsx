a37cfa6ed1f60b35d641b4aaf0799498174e56d9
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-01 18:51:25 -0600

    added desc file comment and blank comp

---
+++ rent-my-drones/src/components/renter/renter-nav-bar.jsx
@@ -0,0 +1,11 @@
+/*
+ * Author : Stephen Aranda
+ * File   : renter-nav-bar.jsx 
+ * Desc   : Single File Component of the navigation bar of the renter user type
+ * */
+
+const RenterNav = () => {
+
+}
+
+export default RenterNav;
