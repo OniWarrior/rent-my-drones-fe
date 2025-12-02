7cc88b5113429b5234b0ed25e29b0e7f7464acc9
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-01 20:28:51 -0600

    added default case

--- rent-my-drones/src/components/logged-in-navigation.jsx
+++ rent-my-drones/src/components/logged-in-navigation.jsx
@@ -1,23 +1,26 @@
 
 
 import RenterNav from "./renter/renter-nav-bar";
+import { useNavigate } from "react-router";
 import { connect } from "react-redux";
 import '../styles/nav-comp.css'
 
 const LoggedInNavigation = (props) => {
+    const navigate = useNavigate();
 
     // get the role 
     const role = localStorage.getItem('role');
 
     // check what role
     switch (role) {
-        case "Renter": return (<div className="nav-container">
+        case "Renter": return (
+            <div className="nav-container">
+                <RenterNav />
+            </div>);
+        default:
+            navigate('/');
 
-        </div>)
     }
-
-
-
 
 }
 
