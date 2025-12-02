b73092c374ca1bf948eca128fdbba876eb22f67a
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-01 19:12:55 -0600

    added props

--- rent-my-drones/src/components/logged-in-navigation.jsx
+++ rent-my-drones/src/components/logged-in-navigation.jsx
@@ -3,20 +3,8 @@
 import { useNavigate } from "react-router";
 import '../styles/nav-comp.css'
 
-const LoggedInNavigation = () => {
+const LoggedInNavigation = (props) => {
 
-
-
-    return (
-        <div className='nav-container'>
-            <nav className='navbar'>
-                <button onClick={goToDashboard}>Dashboard</button>
-                <button onClick={goToAvailable}>Available </button>
-                <button onClick={goToRented}>Rented</button>
-                <button onClick={logOut}>Log out</button>
-            </nav>
-        </div>
-    )
 
 }
 
