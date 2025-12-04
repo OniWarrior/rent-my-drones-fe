6cc3c8be9389295904c0af36791e8ce8083ebd3f
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-03 18:02:00 -0600

    connected state to props

--- rent-my-drones/src/components/owner/owner-dashboard.jsx
+++ rent-my-drones/src/components/owner/owner-dashboard.jsx
@@ -6,3 +6,24 @@
 
 import loggedInNavigation from "../logged-in-navigation";
 import { connect } from "react-redux";
+
+const OwnerDashDashboard = (props) => {
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        login: state.loginReducer.login,
+        login_loading: state.loginReducer.loading,
+        login_error: state.loginReducer.error,
+        rented: state.rentedReducer.rentedDrones,
+        rented_loading: state.rentedReducer.loading,
+        rented_error: state.rentedReducer.error
+
+    }
+}
+
+const mapDispatchToProps = {}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(OwnerDashDashboard)
