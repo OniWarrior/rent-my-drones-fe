e49e715af3fe8b132417524c771ecd1b4bb5197e
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-04 10:04:47 -0600

    added desc comment

---
+++ rent-my-drones/src/components/available-card.jsx
@@ -0,0 +1,24 @@
+/*
+ * Author: Stephen Aranda
+ * File  : available-card.jsx 
+ * Desc  : card component for the avaialble component. Used to display the information
+ *       : of a drone and allow a user to rent by pressing the rent button.
+ */
+
+
+
+const Card = (props) => {
+
+    return (
+        <div className="item-card">
+            <img src={props.drone_image} alt='Card of item' />
+            <h2>{props.drone_name}</h2>
+            <p>{props.drone_description}</p>
+            <p>{props.drone_cost}</p>
+            <button>Rent Drone</button>
+        </div>
+    )
+
+}
+
+export default Card
