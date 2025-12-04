5f94483b663cba8e10453cc5d64cf48881852b0d
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-04 10:21:17 -0600

    created form for card

--- rent-my-drones/src/components/available-card.jsx
+++ rent-my-drones/src/components/available-card.jsx
@@ -10,12 +10,16 @@ import '../styles/available-card.css';
 const AvailableCard = (props) => {
 
     return (
-        <div className="item-card">
-            <img src={props.drone_image} alt='Card of item' />
-            <h2>{props.drone_name}</h2>
-            <p>{props.drone_description}</p>
-            <p>{props.drone_cost}</p>
-            <button>Rent Drone</button>
+        <div className="available-card">
+            <form className='available-card-form'>
+                <img src={props.drone_image} alt='Card of item' />
+                <h2>{props.drone_name}</h2>
+                <p>{props.drone_description}</p>
+                <p>{props.drone_cost}</p>
+                <button>Rent Drone</button>
+
+            </form>
+
         </div>
     )
 
