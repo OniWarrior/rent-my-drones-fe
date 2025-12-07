846e2a4df79e3bc5d527ee5a382fe2a38db116a7
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 21:03:19 -0600

    added blank super comp

---
+++ rent-my-drones/src/components/owner/owner-add-drone.jsx
@@ -0,0 +1,16 @@
+/*
+ * Author: Stephen Aranda
+ * File  : owner-add-drone.jsx 
+ * Desc  : single file component of add drone form that allows an owner to add a drone to market place.
+ * */
+
+import '../../styles/add-drone.css';
+import LoggedInNavigation from '../logged-in-navigation';
+
+const AddDrone = () => {
+
+}
+
+
+
+export default AddDrone;
