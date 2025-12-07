57bbfa3968f410bed6638c99ca653ddbca03ff80
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-07 17:49:21 -0600

    added typeError and positive validation

--- rent-my-drones/src/formSchemas/add-drone-form-schema.jsx
+++ rent-my-drones/src/formSchemas/add-drone-form-schema.jsx
@@ -8,7 +8,9 @@ const AddDroneFormSchema = yup.object().shape({
     drone_description: yup.string()
         .required("Please enter description of drone"),
     drone_cost: yup.number()
-        .required("please enter cost of drone"),
+        .typeError("Please enter a drone cost") // This message covers the "" -> NaN error
+        .required("Please enter a drone cost")
+        .positive('please enter a positive cost'),
     drone_url: yup.string()
         .url('Invalid url format')
         .required("Please enter a url of your drone image that is online: 'https://example.com'")
