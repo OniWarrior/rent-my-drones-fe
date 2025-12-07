ffb16db52969a31c6ae736c264902f6fa36ff293
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 20:44:02 -0600

    added schema for add drone form

---
+++ rent-my-drones/src/formSchemas/add-drone-form-schema.jsx
@@ -0,0 +1,17 @@
+import * as yup from 'yup'
+
+
+const AddDroneFormSchema = yup.object().shape({
+    drone_name: yup.string()
+        .trim()
+        .required("Please enter the name of the drone"),
+    drone_description: yup.string()
+        .required("Please enter description of drone"),
+    drone_cost: yup.number()
+        .required("please enter cost of drone"),
+    drone_url: yup.string()
+        .url('Invalid url format')
+        .required("Please enter a url of your drone image that is online")
+
+})
+export default AddDroneFormSchema
