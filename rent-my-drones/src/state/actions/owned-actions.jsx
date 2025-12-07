ee78c4db9f776229d1c8bcc39fb484ab78889113
Stephen Aranda <aranda.stephen88@gmail.com>
2025-12-06 20:19:52 -0600

    added api call to get all of owner's drones

---
+++ rent-my-drones/src/state/actions/owned-actions.jsx
@@ -0,0 +1,29 @@
+import axiosWithAuth from "../../components/utils/axios-with-auth";
+
+export const OWNED_START = 'OWNED_START'
+export const OWNED_SUCCESS = 'OWNED_SUCCESS'
+export const OWNED_FAILURE = 'OWNED_FAILURE'
+
+/*
+ * getOwnersDrones: api call to get all of the owner's drones whether or not they are rented
+ *                     
+ */
+export const getOwnersDrones = () => async (dispatch) => {
+    try {
+        // start action
+        dispatch({ type: OWNED_START });
+
+        // make api call and store response
+        const response = await axiosWithAuth().get('/api/users/all-drones');
+
+        // dispatch success to reducer
+        dispatch({ type: OWNED_SUCCESS, payload: response.data });
+
+    } catch (err) {
+        // dispatch failure to reducer
+        dispatch({ type: OWNED_FAILURE, payload: err.message });
+
+    }
+
+}
+
